5eb596e7564a98156d86e128e7cf1d75368dd131
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-07-02 16:19:55 +0200

    insert in content provider is tested

--- app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
+++ app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.amrarafa.keepn;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.amrarafa.keepn.data.DbOpenHelper;
+import com.example.amrarafa.keepn.data.NoteProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertNote(" new text");
+    }
+
+    private void insertNote(String textNote) {
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(DbOpenHelper.NOTE_TEXT,textNote);
+        Uri uri=getContentResolver().insert(NoteProvider.CONTENT_URI,contentValues);
+        Log.d("MainACtivity", "Inserted note " + uri.getLastPathSegment());
     }
 }
