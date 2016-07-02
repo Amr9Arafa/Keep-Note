fc08a247c7277d1016cb627feda3dce8cb522e00
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-07-02 16:56:39 +0200

    retrieved data successfully

--- app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
+++ app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
@@ -1,22 +1,41 @@
 package com.example.amrarafa.keepn;
 
+import android.annotation.TargetApi;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.amrarafa.keepn.data.DbOpenHelper;
 import com.example.amrarafa.keepn.data.NoteProvider;
 
 public class MainActivity extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         insertNote(" new text");
+        Cursor cursor=getContentResolver().query(NoteProvider.CONTENT_URI,DbOpenHelper.ALL_COLUMNS
+        ,null,null,null,null);
+         String[] from= {DbOpenHelper.NOTE_TEXT};
+        int[] to= {android.R.id.text1};
+        CursorAdapter cursorAdapter= new SimpleCursorAdapter(this,android.R.layout.simple_list_item_1
+        ,cursor,from,to,0);
+
+        ListView listView= (ListView) findViewById(R.id.android_list);
+        listView.setAdapter(cursorAdapter);
+
+
+
     }
 
     private void insertNote(String textNote) {
