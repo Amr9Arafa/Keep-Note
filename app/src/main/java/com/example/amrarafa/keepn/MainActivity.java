17fba528abbd78fe9a505d39cf80278763c02d98
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-07-04 15:52:40 +0200

    create sample notes and delete all notes in options menu are working

--- app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
+++ app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.amrarafa.keepn;
 import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -15,6 +17,7 @@ import android.view.MenuItem;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import com.example.amrarafa.keepn.data.DbOpenHelper;
 import com.example.amrarafa.keepn.data.NoteProvider;
@@ -58,8 +61,58 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         int id = item.getItemId();
 
+        switch (id){
+
+            case R.id.action_create_sample:
+                insertSampleData();
+                break;
+
+            case R.id.action_delete_all:
+                deleteAllNotes();
+                break;
+
+
+
+        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllNotes() {
+
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            //Insert Data management code here
+                            getContentResolver().delete(NoteProvider.CONTENT_URI,null,null);
+                            restatLoader();
+                            Toast.makeText(MainActivity.this,
+                                    getString(R.string.all_deleted),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+    }
+
+
+    private void insertSampleData() {
+        insertNote("multiline \n note");
+        insertNote("single line");
+        insertNote("mvery long note with a lot f texts that exceeds the width of the screen ");
+        restatLoader();
+    }
+
+    private void restatLoader() {
+        getLoaderManager().restartLoader(0,null,this);
     }
 
     private void insertNote(String textNote) {
