85927eb3c971a5aab1e44fc69340c977644baccf
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-07-04 14:23:01 +0200

    implemented loader to retrieve data in background thread

--- app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
+++ app/src/main/java/com/example/amrarafa/keepn/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.amrarafa.keepn;
 
-import android.annotation.TargetApi;
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
@@ -15,25 +16,27 @@ import android.widget.SimpleCursorAdapter;
 import com.example.amrarafa.keepn.data.DbOpenHelper;
 import com.example.amrarafa.keepn.data.NoteProvider;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private CursorAdapter cursorAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         insertNote(" new text");
-        Cursor cursor=getContentResolver().query(NoteProvider.CONTENT_URI,DbOpenHelper.ALL_COLUMNS
-        ,null,null,null,null);
-         String[] from= {DbOpenHelper.NOTE_TEXT};
+
+        String[] from= {DbOpenHelper.NOTE_TEXT};
         int[] to= {android.R.id.text1};
-        CursorAdapter cursorAdapter= new SimpleCursorAdapter(this,android.R.layout.simple_list_item_1
-        ,cursor,from,to,0);
+        cursorAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_1
+        ,null,from,to,0);
 
         ListView listView= (ListView) findViewById(R.id.android_list);
         listView.setAdapter(cursorAdapter);
 
+        getLoaderManager().initLoader(0,null,this);
 
 
     }
@@ -43,5 +46,22 @@ public class MainActivity extends AppCompatActivity {
         contentValues.put(DbOpenHelper.NOTE_TEXT,textNote);
         Uri uri=getContentResolver().insert(NoteProvider.CONTENT_URI,contentValues);
         Log.d("MainACtivity", "Inserted note " + uri.getLastPathSegment());
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this,NoteProvider.CONTENT_URI,null,null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursorAdapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+        cursorAdapter.swapCursor(null);
     }
 }
